bf2beaaddab715c7c2ef9bb47956e03b9eb5eb55
Gopal Ghosh <gopalghosh.dev@gmail.com>
2025-12-10 20:10:07 +0530

    Calendar,popover added

---
+++ src/components/BasicAgeCalculator.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+const BasicAgeCalculator = () => {
+	const [inputdate, setInputDate] = useState<Date | undefined>(undefined);
+	const [open, setOpen] = useState(false);
+
+	return <></>;
+};
+
+export default BasicAgeCalculator;
